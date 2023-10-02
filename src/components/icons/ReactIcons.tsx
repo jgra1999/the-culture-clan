2c2ee6a2406defb42da2936c28b06baf6dc9a419
Jose Rojas <jgra11.2010@gmail.com>
2023-10-02 16:27:50 -0400

    react icons file created

--- src/components/icons/ReactIcons.tsx
+++ src/components/icons/ReactIcons.tsx
@@ -29,6 +29,7 @@ export const PencilIcon = () => (
 		<path d='M13.5 6.5l4 4' />
 	</svg>
 )
+
 export const TrashIcon = () => (
 	<svg
 		xmlns='http://www.w3.org/2000/svg'
@@ -46,5 +47,25 @@ export const TrashIcon = () => (
 		<path d='M14 11l0 6' />
 		<path d='M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12' />
 		<path d='M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3' />
+	</svg>
+)
+
+export const IconLogout = () => (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		className='flex-shrink w-6 h-6'
+		width={24}
+		height={24}
+		viewBox='0 0 24 24'
+		strokeWidth='2'
+		stroke='currentColor'
+		fill='none'
+		strokeLinecap='round'
+		strokeLinejoin='round'
+	>
+		<path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+		<path d='M10 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2'></path>
+		<path d='M15 12h-12l3 -3'></path>
+		<path d='M6 15l-3 -3'></path>
 	</svg>
 )
