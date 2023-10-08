de2b0f1a4305dd6c047d0227c01cd72ace0b9bb7
Jose Rojas <jgra11.2010@gmail.com>
2023-10-08 18:31:37 -0400

    Comentados los inputs para evitar error

--- src/components/form/ResetPassForm.tsx
+++ src/components/form/ResetPassForm.tsx
@@ -18,7 +18,7 @@ export default function ResetPassForm() {
 	return (
 		<form className='flex flex-col gap-y-4'>
 			<div className='grid grid-cols-1 gap-y-5 gap-x-8'>
-				<InputForm label='Correo Electrónico' type='email' />
+				{/* <InputForm label='Correo Electrónico' type='email' /> */}
 			</div>
 			<ButtonForm text='Enviar Email' />
 		</form>
