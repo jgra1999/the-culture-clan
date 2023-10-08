f6374ddc11edfc8949fc2b90c3ff04330185cf9b
Jose Rojas <jgra11.2010@gmail.com>
2023-10-08 17:32:21 -0400

    Adaptador para usar SSR agregado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
-	output: 'hybrid',
-	image: {
-		domains: ['cloudinary.com'],
-		remotePatterns: [{ protocol: 'https' }]
-	}
-})
+  integrations: [tailwind(), react()],
+  output: 'hybrid',
+  image: {
+    domains: ['cloudinary.com'],
+    remotePatterns: [{
+      protocol: 'https'
+    }]
+  },
+  adapter: netlify()
+});
 
 //TODO: Si no lo utilizare, ver documentacion para desactivar error de produccion causado por sharp
