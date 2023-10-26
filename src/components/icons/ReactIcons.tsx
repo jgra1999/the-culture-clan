bb7209c0ddf559bbe2cc52be51cca975924a2afa
Jose Rojas <jgra11.2010@gmail.com>
2023-10-26 19:55:14 -0400

    icono de corazon agregado

--- src/components/icons/ReactIcons.tsx
+++ src/components/icons/ReactIcons.tsx
@@ -250,3 +250,19 @@ export const IconChevronLeft = ({ styles = 'w-6 h-6' }) => (
 		<path d='M15 6l-6 6l6 6'></path>
 	</svg>
 )
+
+export const IconHeart = () => (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		className='w-7 h-7'
+		viewBox='0 0 24 24'
+		strokeWidth={1.25}
+		stroke='currentColor'
+		fill='none'
+		strokeLinecap='round'
+		strokeLinejoin='round'
+	>
+		<path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+		<path d='M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572'></path>
+	</svg>
+)
