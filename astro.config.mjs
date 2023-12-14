779bda47ff301b048c7f477a1b60161245fc0fa9
Jose Rojas <jgra11.2010@gmail.com>
2023-12-13 23:33:28 -0400

    etiquetas SEO y Google analytics agregados

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify/functions';
 
-import netlify from "@astrojs/netlify/functions";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), partytown()],
   output: 'hybrid',
   image: {
     domains: ['cloudinary.com'],
