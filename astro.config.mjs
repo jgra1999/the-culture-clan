dd81313c1fa9ed135e8445f3ccc8fc05a4e85fe9
Jose Rojas <jgra11.2010@gmail.com>
2024-10-06 21:28:01 -0400

    Instales Eslint and Heroicons libraries

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: netlify()
+	integrations: [react(), tailwind()],
+	output: 'hybrid',
+	adapter: netlify()
 })
