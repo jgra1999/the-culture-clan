c4902e8cd431bb3febab72dfbf376c4e7e78c5ae
Jose Rojas <jgra11.2010@gmail.com>
2024-10-13 13:05:03 -0400

    Created pages and components of the gastos

---
+++ src/components/admin/bills/DataTable.tsx
@@ -0,0 +1,166 @@
+import { useEffect, useState } from 'react'
+import { supabase } from '@/supabase/client'
+/* icons */
+import { TrashIcon, EyeIcon, PencilIcon } from '@heroicons/react/20/solid'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline'
+
+import { SearchInput } from '../ui/SearchInput'
+
+const headers = ['Fecha', 'Descripción', 'Dolares', 'Pesos']
+
+export function BillsTable() {
+	const [data, setData] = useState<any>([])
+	const [Search, setSearch] = useState('')
+	const [page, setPage] = useState(1)
+	const [from, setFrom] = useState(0)
+	const [to, setTo] = useState(4)
+	const ITEMS_PER_PAGE = 4
+
+	const fetchData = async () => {
+		try {
+			console.log('try')
+			const { data, error } = await supabase
+				.from('bills')
+				.select('*')
+				.range(from, to)
+
+			if (error) console.log(error)
+
+			console.log(data)
+			setData(data)
+		} catch (error) {
+			return Response.json(
+				{
+					message: error
+				},
+				{
+					status: 500
+				}
+			)
+		}
+	}
+
+	const deleteProduct = async (id: string) => {
+		// const { error } = await supabase.from('products').delete().eq('id', id)
+		// if (error) {
+		// 	toast(<ErrorToast message='No se pudo eliminar el producto' />)
+		// } else {
+		// 	toast(<SuccessToast message='Producto eliminado correctamente' />)
+		// }
+	}
+
+	/* Pagination Next and Prev button */
+	const handleNextButton = () => {
+		let from = page * ITEMS_PER_PAGE
+		let to = from + ITEMS_PER_PAGE
+
+		if (page > 0) {
+			from += 1
+			to += 1
+		}
+
+		setFrom(from)
+		setTo(to)
+		setPage(page + 1)
+	}
+
+	const handlePrevButton = () => {
+		let oldFrom = from - ITEMS_PER_PAGE
+		let oldTo = to - ITEMS_PER_PAGE
+
+		if (page === 2) {
+			oldFrom -= 2
+			oldTo -= 1
+		}
+
+		setFrom(oldFrom)
+		setTo(oldTo)
+		setPage(page - 1)
+	}
+
+	const handleSearchBill = (value: string) => {
+		setSearch(value)
+	}
+
+	useEffect(() => {
+		fetchData()
+	}, [from, to, Search])
+	return (
+		<>
+			<div className='flex flex-col md:flex-row gap-y-5 w-full items-center justify-between my-10'>
+				<div className='bg-darkGray text-white py-2 w-36  active:opacity-80 text-sm lg:hover:opacity-80 text-center rounded-lg'>
+					<a href='/admin/gastos/agregar-registro' className=''>
+						Agregar Registro
+					</a>
+				</div>
+				<div>
+					<SearchInput
+						text='Ingresa el nombre de la franela...'
+						handleSearch={handleSearchBill}
+					/>
+				</div>
+			</div>
+			<table className='w-full text-sm text-left text-grayText shadow-md sm:rounded-lg'>
+				<thead className='text-xs text-grayText uppercase bg-lightGray'>
+					<tr>
+						{headers.map((header, index) => (
+							<th scope='col' className='px-6 py-3' key={index}>
+								{header}
+							</th>
+						))}
+						<th scope='col' className='px-6 py-3'>
+							<span className='sr-only'>Acciones</span>
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					{data ? (
+						<>
+							{data.map((item: any) => (
+								<tr key={item.id} className='border-b border-mediumGray '>
+									<th className='px-6 py-4'>{item.date}</th>
+									<td className='px-6 py-4'>{item.description}</td>
+									<td className='px-6 py-4'>${item.dollar_amount}</td>
+									<td className='px-6 py-4'>${item.pesos_amount}</td>
+									<td className='px-6 py-4'>
+										<div className='flex gap-x-2 justify-end'>
+											<a href={`/admin/gastos/actualizar/${item.id}`}>
+												<PencilIcon className='w-5 opacity-70 hover:opacity-100' />
+											</a>
+											<button onClick={() => deleteProduct(item.id)}>
+												<TrashIcon className='w-5 opacity-70 hover:opacity-100' />
+											</button>
+										</div>
+									</td>
+								</tr>
+							))}
+						</>
+					) : (
+						<tr className='border-b border-lightGray '>
+							<td className='text-center py-10 text-lg'>No hay datos registrados</td>
+						</tr>
+					)}
+				</tbody>
+			</table>
+			<div className='flex justify-between mt-5'>
+				<button
+					className='flex items-center p-4 opacity-50 hover:opacity-100'
+					disabled={page === 1}
+					onClick={handlePrevButton}
+				>
+					<ChevronLeftIcon className='w-5' />
+					Anterior
+				</button>
+
+				<button
+					className='flex items-center p-4 opacity-50 hover:opacity-100'
+					disabled={data && data.length < 5}
+					onClick={handleNextButton}
+				>
+					Siguiente
+					<ChevronRightIcon className='w-5' />
+				</button>
+			</div>
+		</>
+	)
+}
---
+++ src/components/admin/bills/Form.tsx
@@ -0,0 +1,170 @@
+import { useEffect, useState } from 'react'
+import type { Database } from '@/types/supabase'
+
+/* libraries */
+import toast, { Toaster } from 'react-hot-toast'
+import axios from 'axios'
+import { supabase } from '@/supabase/client'
+/* components */
+import ErrorToast from '@/components/ui/toasts/ErrorToast'
+import SuccessToast from '@/components/ui/toasts/SuccessToast'
+// import { slugify } from '@/utils-slugify'
+import SubmitButton from '@/components/ui/forms/SubmitButton'
+
+export function BillsForm({ id }: { id?: string }) {
+	const [register, setRegister] = useState({
+		title: '',
+		description: '',
+		dollar_amount: 0,
+		pesos_amount: 0,
+		date: ''
+	})
+
+	if (id) {
+		const getRegisterData = async () => {
+			const { data, error } = await supabase.from('bills').select('*').eq('id', id)
+
+			if (error) console.log(error)
+
+			if (data) {
+				setRegister(data[0])
+				console.log(data)
+			}
+		}
+
+		useEffect(() => {
+			getRegisterData()
+		}, [])
+	}
+
+	const handleChange = (
+		e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
+	) => {
+		const { name, value } = e.target
+		setRegister({ ...register, [name]: value })
+	}
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		const { title, description, dollar_amount, pesos_amount, date } = register
+
+		if (id) {
+			const { data, error } = await supabase
+				.from('bills')
+				.update({
+					title,
+					description,
+					dollar_amount: Math.round(register.pesos_amount / 3800),
+					pesos_amount,
+					date
+				})
+				.eq('id', id)
+
+			if (error) {
+				toast.custom(<ErrorToast message={error?.message} />)
+			} else {
+				toast.custom(<SuccessToast message='Registro editado' />)
+				setInterval(() => {
+					window.location.replace('/admin/gastos')
+				}, 2000)
+			}
+		} else {
+			const { data, error } = await supabase.from('bills').insert([
+				{
+					title,
+					description,
+					dollar_amount: Math.round(register.pesos_amount / 3800),
+					pesos_amount,
+					date
+				}
+			])
+			if (error) {
+				toast.custom(<ErrorToast message={error.message} />)
+			} else {
+				toast.custom(<SuccessToast message='Ingreso registrado' />)
+				setInterval(() => {
+					window.location.replace('/admin/gastos')
+				}, 2000)
+			}
+		}
+	}
+
+	return (
+		<>
+			<form onSubmit={handleSubmit} className='flex flex-col gap-y-8'>
+				<div className='grid grid-cols-1 md:grid-cols-2 gap-8 '>
+					<div className='flex flex-col gap-y-2 text-left relative'>
+						<div className='flex flex-col gap-y-2 text-left relative'>
+							<label htmlFor='title' className='text-grayText'>
+								Titulo
+							</label>
+							<input
+								type='text'
+								name='title'
+								onChange={handleChange}
+								value={register ? register.title : ''}
+								className='bg-lightGray outline-none opacity-75 focus:opacity-100 border border-mediumGray py-2 px-3 rounded-lg'
+							/>
+						</div>
+					</div>
+					<div className='flex flex-col gap-y-2 text-left relative'>
+						<label htmlFor='date' className='text-grayText'>
+							Fecha
+						</label>
+						<input
+							type='date'
+							name='date'
+							onChange={handleChange}
+							value={register ? register.date : ''}
+							className='bg-lightGray outline-none opacity-75 focus:opacity-100 border border-mediumGray py-2 px-3 rounded-lg'
+						/>
+					</div>
+
+					<div className='flex flex-col gap-y-2 text-left relative'>
+						<label htmlFor='pesos_amount' className='text-grayText'>
+							Cantidad (Pesos)
+						</label>
+						<input
+							type='number'
+							step={0.01}
+							name='pesos_amount'
+							onChange={handleChange}
+							value={register ? register.pesos_amount : ''}
+							className='bg-lightGray outline-none opacity-75 focus:opacity-100 border border-mediumGray py-2 px-3 rounded-lg'
+						/>
+					</div>
+
+					<div className='flex flex-col gap-y-2 text-left relative'>
+						<label htmlFor='dollar_amount' className='text-grayText'>
+							Cantidad (Dolares)
+						</label>
+						<input
+							type='number'
+							step={0.01}
+							name='dollar_amount'
+							onChange={handleChange}
+							value={register ? Math.round(register.pesos_amount / 3800) : ''}
+							className='bg-lightGray outline-none opacity-75 focus:opacity-100 border border-mediumGray py-2 px-3 rounded-lg'
+						/>
+					</div>
+
+					<div className='flex flex-col text-left gap-y-2 md:col-span-2 relative'>
+						<label htmlFor='description' className='text-grayText'>
+							Descripción
+						</label>
+						<textarea
+							rows={5}
+							value={register ? register.description : ''}
+							name='description'
+							onChange={handleChange}
+							className='bg-lightGray outline-none opacity-75 focus:opacity-100 border border-mediumGray py-2 px-3 rounded-lg resize-none'
+						></textarea>
+					</div>
+				</div>
+				<SubmitButton text={id ? 'Actualizar Registro' : 'Agregar Registro'} />
+			</form>
+			<Toaster />
+		</>
+	)
+}
